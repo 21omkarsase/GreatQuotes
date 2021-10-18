bdc2e1b6f2bd3c89e08830fb9bd886cb92ed0b1f
Omkar <saseomkar214@gmail.com>
2021-10-18 15:47:56 +0530

    third commit

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -7,7 +7,7 @@ const MainNavigation = () => {
       <nav className={classes.nav}>
         <ul>
           <li>
-            <NavLink to="/quotes" activeClassName={classes.active}>
+            <NavLink to="/GreatQuotes" activeClassName={classes.active}>
               All Quotes
             </NavLink>
           </li>
